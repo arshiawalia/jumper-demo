ba1c164667881f4ee8187dbf26f217445bc09e09
arshiawalia <arshiawalia1@gmail.com>
2020-11-27 11:40:28 +0000

    + Db set & get

--- controller.py
+++ controller.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from pusher import Pusher
+from google.cloud import datastore
+import datetime
 
 app = Flask(__name__)
 pusher = Pusher(app_id='1112407',
@@ -8,21 +10,43 @@ pusher = Pusher(app_id='1112407',
   cluster='ap2',
   ssl=True)
 
+client = datastore.Client("jumper-chat-demo-296719")
+
 @app.route('/')
 def index():
     return render_template('sender.html')
 
+def insert(message):
+    with client.transaction():
+        key = client.key("Task")
+        task = datastore.Entity(key=key)
+
+        task.update(
+            {
+                "created_ts": datetime.datetime.now(),
+                "message": message,
+            }
+        )
+        client.put(task)
+
 @app.route('/receiver')
-def msg_html():
-    return render_template('receiver.html')
+def receiver():
+    records = fetch_records(10)
+    return render_template('receiver.html', records=records)
 
 @app.route('/messages', methods=['POST'])
 def message():
-    data = request.form
-    pusher.trigger(u'my-msg', u'my-event', {
-    	'message': data['messages']
-    })
+    msg = {'message': request.form['messages']}
+    
+    pusher.trigger(u'my-msg', u'my-event', msg)
+
+    insert(msg)
     return "message sent"
+
+def fetch_records(limit):
+    query = client.query(kind='Task')
+    query.order = ['-created_ts']
+    return query.fetch(limit=limit)
 
 if __name__ == '__main__':
     app.run(debug=True)
